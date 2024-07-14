0b0b2768e8abd4d3eb520fe1b29229693a23084c
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-07-15 02:53:19 +0900

    Time: 88 ms (31.48%) | Memory: 20 MB (11.79%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,37 +1,24 @@
+class ListNode:
+    def __init__ (self, val=0,next=None):
+        self.val = val
+        self.next = next 
+        
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists or len(lists) == 0:
-            return None
+        root = result = ListNode(None)
+        heap = []
+
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap, (lists[i].val, i , lists[i]))
         
-        while len(lists) > 1:
-            mergedLists = []
+        while heap:
+            node = heapq.heappop(heap)
+            i = node[1]
+            result.next = node[2]
 
-            for i in range(0, len(lists), 2):
-                lst1 = lists[i]
-                if (i+1) < len(lists):
-                    lst2 = lists[i+1] 
-                else:
-                    lst2 = None
-                mergedLists.append(self.mergeList(lst1,lst2)) 
-            lists = mergedLists
-
-        return lists[0] # return head of lists
-
-    #Merge Two list Node
-    def mergeList(self,lst1:ListNode,lst2:ListNode) -> ListNode:
-        tmp = ListNode()
-        tail = tmp
-
-        while lst1 and lst2:
-            if lst1.val  < lst2.val:
-                tail.next = lst1
-                lst1 = lst1.next
-            else:
-                tail.next = lst2
-                lst2 = lst2.next
-            tail = tail.next
-        if lst1:
-            tail.next = lst1
-        if lst2:
-            tail.next = lst2
-        return tmp.next
+            result = result.next
+            if result.next:
+                heapq.heappush(heap, (result.next.val, i, result.next))
+        
+        return root.next
