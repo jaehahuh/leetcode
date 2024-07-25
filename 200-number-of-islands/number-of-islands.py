26a0c76ef44abaaf460ad4326b98d0a1e5560bf6
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-07-25 15:38:05 +0900

    Time: 228 ms (84.62%) | Memory: 18.8 MB (70.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,7 +2,7 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        
+
         count = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -12,13 +12,13 @@ class Solution:
                     count += 1
         return count
 
-    def dfs(self, grid, x, y):
-        if x < 0 or len(grid) <= x or y < 0 or len(grid[0]) <= y or grid[x][y] != "1":
+    def dfs(self, grid, v, h):
+        if v < 0 or len(grid) <= v or h < 0 or len(grid[0]) <= h or grid[v][h] != "1":
             return
-        grid[x][y] = "visited"
-        
+        grid[v][h] = "visited"
+
         #search island horizontally and vertically
-        self.dfs(grid,x-1,y)
-        self.dfs(grid,x+1,y) 
-        self.dfs(grid,x,y-1) 
-        self.dfs(grid,x,y+1) 
+        self.dfs(grid, v-1, h)
+        self.dfs(grid, v+1, h) 
+        self.dfs(grid, v, h-1) 
+        self.dfs(grid, v, h+1) 
