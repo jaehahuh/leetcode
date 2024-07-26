005630bf36d3b837c37dd4d3cc4d74f8670eddfb
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-07-27 01:59:54 +0900

    Time: 35 ms (57.89%) | Memory: 16.6 MB (28.32%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -9,7 +9,7 @@ class Solution:
             '5':["j","k","l"], '6':["m","n","o"], '7':["p","q","r","s"], 
             '8':["t","u","v"], '9':["w","x","y","z"],
             }
-     
+
         def backtracking(i, cur_ch):
             if len(cur_ch) == len(digits):
                 result.append(cur_ch)
