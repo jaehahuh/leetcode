11ca23b55a8a3ba57501ac0ef265fb26ed068d23
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-08-05 12:47:43 +0900

    Time: 148 ms (23.68%) | Memory: 17.1 MB (16.94%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.py
@@ -6,7 +6,7 @@ class Solution:
        
         for i in range(n-2):
             for j in range(n-2):
-                #find the largest value in every contiguous 3 x 3
+                #find the largest value in every contiguous 3 x 3 matrix
                 for column in range(i, i+3):
                     for row in range(j, j+3):
                         result[i][j] = max(result[i][j], grid[column][row])
