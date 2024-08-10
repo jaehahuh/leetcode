d72539de1abd7747f5167d503a9bab9036aaa81e
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-08-10 21:33:45 +0900

    Time: 40 ms (98.99%) | Memory: 16.7 MB (20.51%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,10 +1,24 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        count_lst = []
-        for i in range(len(nums)):
-            count = 0
-            for j in range(len(nums)):
-                if nums[i] > nums[j]:
-                    count += 1
-            count_lst.append(count)
-        return count_lst
+        #counting sort method
+        #create a frequency array (constrains: 0 < nums[i] < 100), so set freq list size as 101 
+        freq = [0] * 101
+
+        #count frequency
+        for num in nums:
+            freq[num] += 1
+        
+        
+        #convert frequency array to prefix sum array
+        for i in range(1, 101):
+            freq[i] += freq[i - 1]
+        
+
+        result = []
+        for num in nums:
+            if num == 0:
+                result.append(0)
+            else:
+                result.append(freq[num - 1])
+        
+        return result
