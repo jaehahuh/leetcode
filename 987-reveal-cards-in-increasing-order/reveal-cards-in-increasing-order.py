6686656432ba4f64984bbf4995a7bdec85c8e088
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-08-12 13:30:07 +0900

    Time: 47 ms (49.50%) | Memory: 16.7 MB (82.71%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -8,11 +8,11 @@ class Solution:
         #this queue helps in managing the position of cards in the final result
         queue = collections.deque(range(len(deck)))
 
-        #revealing the Cards:
+        #revealing the cards:
         for num in deck:
             i = queue.popleft()
             result[i] = num
-            # if there are still indices left in the queue
+            #if there are still indices left in the queue
             if queue:
                 #pop the next index and append it to the end of the queue
                 #this simulates the "skip" operation where we move the next index to the back
