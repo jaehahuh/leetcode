1ff1e93285de6e7818ea9103f5e04d3ab0f4fe9a
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-08-12 14:08:47 +0900

    Time: 65 ms (78.97%) | Memory: 19.5 MB (67.97%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.py
@@ -4,19 +4,12 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    #get GCD between two numbers
-    def gcd(num1, num2):
-        for i in range(min(num1,num2),0,-1):
-            if num1 % i == 0 and num2 % i == 0:
-                return i
-
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
         p = head
-        while p.next:
-            gcd_num = gcd(p.val, p.next.val)
-            tmp = ListNode()
-            tmp.val = gcd_num
-            tmp.next = p.next
+        while p and p.next:
+            gcd_num = math.gcd(p.val, p.next.val)
+            tmp = ListNode(gcd_num, p.next)
             p.next = tmp
+            # move to the next pair of nodes.
             p = tmp.next        
         return head
