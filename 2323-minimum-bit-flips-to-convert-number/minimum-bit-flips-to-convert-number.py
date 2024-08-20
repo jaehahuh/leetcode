3f5b7aca4c422b756ce32bef48393b1f846c3097
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-08-20 11:35:28 +0900

    Time: 29 ms (90.86%) | Memory: 16.5 MB (23.79%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        #xor operation
+        #use xor method
         bit_flips = start ^ goal
         count = bin(bit_flips).count('1')
         
