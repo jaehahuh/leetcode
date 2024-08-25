79597328cde732833cb822696f789453d8a5cc02
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2024-08-26 00:26:15 +0900

    Time: 41 ms (35.86%) | Memory: 16.6 MB (20.83%) - LeetSync

--- 2406-decode-the-message/decode-the-message.py
+++ 2406-decode-the-message/decode-the-message.py
@@ -2,6 +2,7 @@ class Solution:
     def decodeMessage(self, key: str, message: str) -> str:
         key_dic = {}
         n = ord('a')
+        
         #build substitution table
         for ch in key:
             if ch != ' ' and ch not in key_dic:
