efbf327c8dec8af41285090eb54f69f61c93f1b3
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-01-07 03:21:58 +0900

    Time: 0 ms (100.00%) | Memory: 18.3 MB (23.13%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            mid = left + (right-left)//2 #to prevent overflow when calculating the mid index in binary search
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid
+        return left
