e0245e41f1601df6b7439bbb179dc1baa066f015
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-02-12 21:47:07 +0900

    Time: 518 ms (23.38%) | Memory: 33.5 MB (78.99%) - LeetSync

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        digit_sum_dict = defaultdict(list) #Dictionary to hold lists of numbers with the same digit sum
+        for i, num in enumerate(nums):
+            sum_digits = sum(int(digit) for digit in str(num))
+            # Append the number to the list corresponding to its digit sum
+            digit_sum_dict[sum_digits].append(num) 
+
+        # Initialize max_sum to -1, which will hold the maximum sum found
+        max_sum = -1
+
+        # Calculate the maximum sum of the two largest numbers with the same digit sum
+        for num_list in  digit_sum_dict.values():
+            if len(num_list) > 1:
+                num_list.sort(reverse=True) 
+                max_sum = max(max_sum, num_list[0] + num_list[1]) # Sum the two largest values
+
+        return max_sum
