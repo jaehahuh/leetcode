a2959779a4c2209f035f2432f13687cba1dd0f04
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-03-13 17:34:58 +0900

    Time: 1279 ms (16.41%) | Memory: 64.2 MB (23.15%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        result = -1
+        left, right = 0, len(queries) # Initialize binary search bounds
+
+        while left <= right:
+            mid = (left + right) // 2
+            decrement_array = [0] * (len(nums) + 1)  # Create an array to track decrements
+
+
+            for i in range(mid): 
+                start, end, val = queries[i]
+                decrement_array[start] += val
+                if end + 1 < len(nums): # Check if the end index + 1 is within bounds
+                    decrement_array[end + 1] -= val
+
+            current_decrement = 0 
+            can_make_zero = True  #Flag to check if zero array is possible
+            
+            # Check if the nums array can be made zero with the current decrements
+            for i in range(len(nums)):
+                current_decrement += decrement_array[i]
+                if nums[i] > current_decrement:
+                    can_make_zero = False
+                    break
+
+            if can_make_zero:
+                result = mid
+                right = mid - 1 # Search in the left half for a smaller k
+            else:
+                left = mid + 1 # Search in the right half for a larger k
+
+        return result
