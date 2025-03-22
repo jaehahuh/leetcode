00c57a79cb7749695d2290c963b766be8807ad08
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-03-22 18:10:00 +0900

    Time: 113 ms (8.03%) | Memory: 18.4 MB (33.84%) - LeetSync

---
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.py
@@ -0,0 +1,62 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))  # Initialize the parent of each node. Each node is its own parent initially
+        self.size = [1] * n  # Initialize the size of each node. Initially, all nodes have size 1
+        self.edge_count = [0] * n  # Initialize the edge count for each component. All start at 0
+    
+    # Find the root of x
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        # If the roots are different, connect them
+        if root_x != root_y:
+            # Attach the smaller tree under the larger tree
+            if self.size[root_x] < self.size[root_y]:
+                self.parent[root_x] = root_y
+                self.size[root_y] += self.size[root_x]
+                self.edge_count[root_y] += self.edge_count[root_x]
+                return root_y
+            else:
+                self.parent[root_y] = root_x
+                self.size[root_x] += self.size[root_y]
+                self.edge_count[root_x] += self.edge_count[root_y]
+                return root_x
+            
+            return root_x
+    
+    # Increment the edge count for the component of x
+    def increment_edges(self, x):
+        root = self.find(x)
+        self.edge_count[root] += 1
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf = UnionFind(n)
+
+        for a, b in edges:
+            uf.union(a,b) # Connect nodes a and b
+            uf.increment_edges(a)  # Increment edge count for node a
+            uf.increment_edges(b) # Increment edge count for node b
+        
+        complete_components = 0
+        seen = set() # Track already checked roots
+
+        # Check the number of vertices and edges for each component
+        for i in  range(n):
+            root = uf.find(i)
+            if root not in seen: # If this root hasn't been checked yet
+                seen.add(root)
+                vertices_count = uf.size[root] # Get the number of vertices in this component
+                edges_count = uf.edge_count[root] // 2 # Divide by 2 since each edge is counted twice
+                
+                # Check the complete connection condition
+                if edges_count == (vertices_count * (vertices_count - 1)) // 2:
+                    complete_components += 1
+
+        return complete_components 
