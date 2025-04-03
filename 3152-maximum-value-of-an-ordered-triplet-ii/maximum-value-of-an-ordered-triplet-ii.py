95c70ff222bdd6d558e1f95a2080032e2a439acd
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-04-03 14:17:30 +0900

    Time: 180 ms (56.65%) | Memory: 29.5 MB (67.92%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.py
@@ -1,22 +1,12 @@
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        n = len(nums)
-        max_i = [0] * n
-        max_k = [0] * n
+        prefix_max = nums[0]
+        max_diff = 0
+        result = 0
 
-        # Calculate max_i array (maintain maximum from the left)
-        max_i[0] = nums[0]
-        for i in range(n):
-            max_i[i] = max(max_i[i - 1], nums[i])
-
-        # Calculate max_k array (maintain maximum from the right)
-        max_k[n-1] = nums[n-1]
-        for k in range(n - 2, -1, -1):
-            max_k[k] = max(max_k[k + 1] , nums[k])
-
-        max_value = 0
-        for j in range(1, n-1):
-            value_of_triplet = (max_i[j - 1] - nums[j]) * max_k[j + 1]
-            max_value = max(max_value, value_of_triplet)
-
-        return max_value 
+        for k in range(1, len(nums)):
+            result = max(result, max_diff * nums[k])
+            prefix_max = max(prefix_max, nums[k])
+            max_diff = max(max_diff, prefix_max - nums[k])
+            
+        return result
