2997f98e4b82fb5a815cfe273a1c2239804495f9
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-04-14 15:14:31 +0900

    Time: 174 ms (92.47%) | Memory: 17.6 MB (85.75%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.py
+++ 1656-count-good-triplets/count-good-triplets.py
@@ -1,9 +1,22 @@
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
         count = 0
-        for i in range(len(arr)-2):
-            for j in range(i+1, len(arr)-1):
-                for k in range(j+1, len(arr)):
-                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
-                        count += 1
+        n = len(arr)
+        prefix_count = [0] * 1001
+
+        for j in range(n-1):
+            for k in range(j+1, n):
+                if abs(arr[j] - arr[k]) <= b:
+                    right_boundary = min(arr[j] + a, arr[k] + c) 
+                    left_boundary = max(arr[j] - a, arr[k] - c)
+
+                    right_boundary = min(right_boundary, 1000)
+                    left_boundary =  max(left_boundary , 0)
+
+                    if left_boundary <= right_boundary:
+                        count += prefix_count[right_boundary] - (0 if left_boundary == 0 else prefix_count[left_boundary-1])
+
+            for index in range(arr[j], 1001):
+                prefix_count[index] += 1
+                
         return count
