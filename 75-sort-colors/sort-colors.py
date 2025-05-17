265ee47f481314a4239e51efb1a3431760441165
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-05-17 11:51:45 +0900

    Time: 0 ms (100.00%) | Memory: 18 MB (11.82%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,4 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        start = 0
+        pointer = 0
+        end = len(nums) - 1
+
+        while pointer <= end:
+            if nums[pointer] == 0:
+                nums[start], nums[pointer] = nums[pointer], nums[start]
+                start += 1
+                pointer += 1
+            
+            elif nums[pointer] == 1:
+                pointer += 1
+            
+            else: #nums[pointer] == 2
+                nums[end], nums[pointer] = nums[pointer], nums[end]
+                end -= 1
