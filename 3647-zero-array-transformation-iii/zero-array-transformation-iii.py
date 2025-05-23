d504c5c7ce99fe740e9cf8616721efe5872cff0e
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-05-23 09:40:07 +0900

    Time: 708 ms (10.55%) | Memory: 61.2 MB (73.83%) - LeetSync

---
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
+        q = collections.deque(sorted(queries))
+        available = SortedList()  
+        running = SortedList()  
+
+        for i, num in enumerate(nums):
+            while q and q[0][0] <= i:
+                available.add(q.popleft()[1])
+            while running and running[0] < i:
+                running.pop(0)
+            while num > len(running):
+                if not available or available[-1] < i:
+                    return -1
+                running.add(available.pop())
+
+        return len(available)
