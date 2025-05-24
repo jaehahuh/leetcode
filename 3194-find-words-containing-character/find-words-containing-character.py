955c52c540402d1e0a4aa2028c792e63ecf050d4
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-05-24 10:55:19 +0900

    Time: 3 ms (30.20%) | Memory: 17.5 MB (99.30%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.py
+++ 3194-find-words-containing-character/find-words-containing-character.py
@@ -1,11 +1,9 @@
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
-        output = []
+        result = []
         for i in range(len(words)):
-            for char in words[i]:
-                #If that one char is equal to x, put x in the list and break the loop.
-                if char == x:
-                    output.append(i)
+            for ch in words[i]:
+                if ch == x:
+                    result.append(i)
                     break
-
-        return output
+        return result
