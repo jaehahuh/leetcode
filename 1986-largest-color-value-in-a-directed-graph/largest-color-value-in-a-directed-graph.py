6b17c83792f990e9ce9cfad5875a6e1d7bd70804
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-05-26 20:37:31 +0900

    Time: 2159 ms (32.14%) | Memory: 84.5 MB (80.67%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
@@ -2,36 +2,34 @@ class Solution:
     def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
         n = len(colors)
         graph = defaultdict(list)
+        indegree = [0] * n
+
         for src, dst in edges:
             graph[src].append(dst)
+            indegree[dst] += 1
         
-        # Return max freq of color
-        def dfs(node):
-            if node in path:
-                return float("inf") #Cycle
-            if node in visited:
-                return 0
-        
-            visited.add(node)
-            path.add(node)
-            
-            color_index = ord(colors[node]) - ord('a')
-            count[node][color_index] = 1
-
-            for neighbor in graph[node]:
-                if dfs(neighbor) == float('inf'):
-                    return float("inf")
-                for c in range(26):
-                    count[node][c] = max(count[node][c], (1 if c == color_index else 0) + count[neighbor][c])
-            
-            path.remove(node)
-            return max(count[node])
-
-        result = 0
-        visited = set()
-        path = set()
         count = [[0] * 26 for _ in range(n)] #count[node][color] =  max number of times color c appeared on path to i
-        for i in range(n): 
-            result = max(result, dfs(i))
+        
+        q = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+                count[i][ord(colors[i]) - ord('a')] = 1
+        
+        result = 0
+        visited = 0
 
-        return -1 if result == float("inf") else result
+        while q:
+            node = q.popleft()
+            visited += 1
+            for neighbor in graph[node]:
+                for c in range(26):
+                    # If color is same, then add 1
+                    add = 1 if c == ord(colors[neighbor]) - ord('a') else 0
+                    count[neighbor][c] = max(count[neighbor][c], count[node][c] + add)
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+            result = max(result, max(count[node]))
+
+        return result if visited == n else -1
