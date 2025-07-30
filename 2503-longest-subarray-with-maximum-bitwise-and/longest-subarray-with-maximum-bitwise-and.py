638e5d7b5e63647150dc3fe7b16c1f6749a963f1
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-07-30 13:34:05 +0900

    Time: 66 ms (49.11%) | Memory: 30.6 MB (47.34%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -1,19 +1,14 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         max_bit_num = max(nums)
-        index_array = []
-        for i, value in enumerate(nums):
-            if value == max_bit_num:
-                index_array.append(i)
-        
-        count = 1
-        max_count = 1
-        for i in range(1,len(index_array)):
-            if (index_array[i-1] + 1) == index_array[i]:
-                count += 1
-            else:
-                max_count = max(count, max_count)
-                count = 1
+        max_length = 0
+        curr_length = 0
 
-        max_count = max(count, max_count)
-        return max_count
+        for num in nums:
+            if num == max_bit_num:
+                curr_length += 1
+                max_length = max(curr_length, max_length)
+            else:
+                curr_length = 0
+        
+        return max_length
