6cfa700c9d4f177347afc1b848a0d0d0f553b00e
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-08-01 13:19:08 +0900

    Time: 0 ms (100.00%) | Memory: 17.5 MB (96.74%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,14 +1,4 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        if len(sentence) < 26:
-            return False
-        
-        count_dict = {}
-        for ch_code in range(ord('a'), ord('z') + 1):
-            alpha = chr(ch_code)
-            count_dict[alpha] = 0
-        
-        for ch in sentence:
-            count_dict[ch] += 1
-        
-        return all(count > 0 for count in count_dict.values())
+        unique_chars = set(sentence)
+        return len(unique_chars) == 26
