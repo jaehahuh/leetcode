5355aeb1096e5813f071778833fb740aa26757dd
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-08-07 21:07:56 +0900

    Time: 1934 ms (17.54%) | Memory: 91.5 MB (71.93%) - LeetSync

---
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maxCollectedFruits(self, fruits: List[List[int]]) -> int:
+        n = len(fruits)
+
+        def getTopLeft():
+            return sum(fruits[i][i] for i in range(n))
+        
+        def getTopRight():
+            dp = [[0] * n for _ in range(n)]
+            dp[0][-1] = fruits[0][-1]
+            for x in range(n):
+                for y in range(n):
+                    if x >= y and (x, y) != (n - 1, n - 1):
+                        continue
+                    for dx, dy in [(1, -1), (1, 0), (1, 1)]:
+                        i = x - dx
+                        j = y - dy
+                        if i < 0 or i == n or j < 0 or j == n:
+                            continue
+                        if i < j < n - 1 - i:
+                            continue
+                        dp[x][y] = max(dp[x][y], dp[i][j] + fruits[x][y])
+            return dp[-1][-1]
+
+        def getBottomLeft():
+            dp = [[0] * n for _ in range(n)]
+            dp[-1][0] = fruits[-1][0]
+            for y in range(n):
+                for x in range(n):
+                    if x <= y and (x, y) != (n - 1, n - 1):
+                        continue
+                    for dx, dy in [(-1, 1), (0, 1), (1, 1)]:
+                        i = x - dx
+                        j = y - dy
+                        if i < 0 or i == n or j < 0 or j == n:
+                            continue
+                        if j < i < n - 1 - j:
+                            continue
+                        dp[x][y] = max(dp[x][y], dp[i][j] + fruits[x][y])
+            return dp[-1][-1]
+
+        return getTopLeft() + getTopRight() + getBottomLeft() - 2 * fruits[-1][-1]
