0d7c1c46858b757bacc592677495d97baca33edf
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-08-13 12:47:35 +0900

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.30%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -2,16 +2,11 @@ class Solution:
     def tribonacci(self, n: int) -> int:
         if n == 0:
             return 0
-        if n == 1 or n == 2:
+        if n <= 2:  # n = 1 or 2
             return 1
 
-        T = [0] * (n + 1)
-
-        T[0] = 0
-        T[1] = 1
-        T[2] = 1
-
+        a, b, c = 0, 1, 1
         for i in range(3, n + 1):
-            T[i] = T[i-1] + T[i-2] + T[i-3]
+            a, b, c = b, c, a + b + c
         
-        return T[n]
+        return c
