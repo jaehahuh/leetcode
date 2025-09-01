00819ae922c973b367c5b3f3b2ba1518c969b09c
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-09-01 18:02:44 +0900

    Time: 1307 ms (23.04%) | Memory: 55.7 MB (7.39%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.py
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.py
@@ -1,32 +1,22 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
-        # calculates percentage increaces in a class
-        def calculate_increase(pass_count, total_count):
-            return (pass_count + 1)/(total_count + 1) - pass_count/total_count
-        
-        # Build max-heap based on increase in pass ratio
+        def gain(p, t):
+            return (p + 1)/(t + 1) - p/t
+
         heap = []
-        for pass_count, total_count in classes:
-            increase = calculate_increase(pass_count, total_count)
-            #Since the heapq module provides a mini-heap structure, 
-            #change it to get the maximum heap by adding minus sign in front of it.
-            #(ratio increase, number of pass students, total number of students)
-            heapq.heappush(heap, (-increase, pass_count, total_count))
-        
-        # Distribute extra students
+        for p, t in classes:
+            heapq.heappush(heap, (-gain(p,t), p, t))
+    
         for _ in range(extraStudents):
-            increase, pass_count, total_count = heapq.heappop(heap)
+            _ , p, t = heapq.heappop(heap)
+            p += 1
+            t += 1
+            heapq.heappush(heap, (-gain(p,t), p, t))
 
-            pass_count += 1
-            total_count += 1
-
-            new_increase = calculate_increase(pass_count,total_count)
-            heapq.heappush(heap, (-new_increase, pass_count, total_count))
+        total = 0.0
+        while heap:
+            _ , p, t = heapq.heappop(heap)
+            total += p/t
         
-        # Calculate the final average pass ratio
-        total_ratio = 0
-        for increase, pass_count, total_count in heap:
-            total_ratio += (pass_count / total_count)
-        
-        return total_ratio/len(classes)
-            
+        max_avg = total/len(classes)
+        return max_avg
