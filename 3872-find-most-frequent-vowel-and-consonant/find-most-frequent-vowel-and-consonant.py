56480af838187cb21490aba4297b3165fe98778c
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-09-13 14:43:47 +0900

    Time: 3 ms (65.94%) | Memory: 18 MB (20.04%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -4,13 +4,13 @@ class Solution:
         vowels = 'aeiou'
         count_dict = Counter(s)
         count_list = sorted(count_dict.items(), key=lambda x:x[1], reverse=True)
+        
         for item in count_list:
             ch, count = item
-            print(ch, count)
             if ch in vowels:
                 sum_freq += count
                 break
-                
+
         for item in count_list:
             ch, count = item
             if ch not in vowels:
