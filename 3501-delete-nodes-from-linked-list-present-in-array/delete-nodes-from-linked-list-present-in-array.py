1e9a25f16648820b9b8f7e2cfd8bd74cc265f7c3
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-11-01 18:06:54 +0900

    Time: 48 ms (93.47%) | Memory: 58.2 MB (87.41%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
@@ -3,24 +3,18 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        #convert the nums list into a set(hash map) for faster lookups
         set_nums = set(nums)
-
-        #make dummy_node
         node = ListNode()
         node.next = head
 
         prev, curr = node, head
-
-        #move linked list
         while curr:
             if curr.val in set_nums:
                 prev.next = curr.next
             else:
-                prev = curr #move prev to curr only if curr node is not removed
+                prev = curr
             curr = curr.next
-
+        
         return node.next
