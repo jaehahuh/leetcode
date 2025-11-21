b678647cf432ccff9f7879e566a94fb4370bcbac
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-11-21 11:19:48 +0900

    Time: 1505 ms (24.80%) | Memory: 18.5 MB (26.60%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,14 +1,14 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        palin_subseq = set() # (mid_character, outer_character)  e.g) o m o 
+        palin_sub = set()
         left = set()
-        right = collections.Counter(s)
+        right = Counter(s)
 
-        for mid_char in s:
-            right[mid_char] -= 1
-            for outer_char in left:
-                if right[outer_char] > 0:  # ensure outer character still exists
-                    palin_subseq.add((mid_char, outer_char)) # add to palindromic subsequences
-            left.add(mid_char)
-
-        return len(palin_subseq)
+        for mid_ch in s:
+            right[mid_ch] -= 1
+            for outer_ch in left:
+                if right[outer_ch] > 0:
+                    palin_sub.add((mid_ch ,outer_ch))
+            left.add(mid_ch)
+    
+        return len(palin_sub)
