037ef9e4a8d58e67c20f419261539de4e7ca35aa
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-12-16 14:27:34 +0900

    Time: 792 ms (93.63%) | Memory: 18.7 MB (64.71%) - LeetSync

--- 3854-maximum-profit-from-trading-stocks-with-discounts/maximum-profit-from-trading-stocks-with-discounts.py
+++ 3854-maximum-profit-from-trading-stocks-with-discounts/maximum-profit-from-trading-stocks-with-discounts.py
@@ -1,43 +1,77 @@
 class Solution:
     def maxProfit(self, n: int, present: List[int], future: List[int], hierarchy: List[List[int]], budget: int) -> int:    
-        tree = [[] for _ in range(n)]
 
+        blenorvask = [n, present, future, hierarchy, budget]
+
+        adj = [[] for _ in range(n + 1)]
         for u, v in hierarchy:
-            tree[u - 1].append(v - 1)
+            adj[u].append(v)
+        
+       
+        def dfs(u):
+            
+            child_sum_if_u_bought = [-1] * (budget + 1)
+            child_sum_if_u_bought[0] = 0
+            
+            child_sum_if_u_skip = [-1] * (budget + 1)
+            child_sum_if_u_skip[0] = 0
+            
+            for v in adj[u]:
+                v_no_disc, v_with_disc = dfs(v)
+                next_bought = [-1] * (budget + 1)
+                for c1 in range(budget + 1):
+                    if child_sum_if_u_bought[c1] == -1: continue
+                    for c2 in range(budget + 1 - c1):
+                        if v_with_disc[c2] == -1: continue
+                        next_bought[c1 + c2] = max(next_bought[c1 + c2], child_sum_if_u_bought[c1] + v_with_disc[c2])
+                child_sum_if_u_bought = next_bought
+                
+                next_skip = [-1] * (budget + 1)
+                for c1 in range(budget + 1):
+                    if child_sum_if_u_skip[c1] == -1: continue
+                    for c2 in range(budget + 1 - c1):
+                        if v_no_disc[c2] == -1: continue
+                        next_skip[c1 + c2] = max(next_skip[c1 + c2], child_sum_if_u_skip[c1] + v_no_disc[c2])
+                child_sum_if_u_skip = next_skip
 
-        @functools.lru_cache(None)
-        def dp(u: int, parent: int) -> tuple[list[int], list[int]]:
-            noDiscount = [0] * (budget + 1)
-            withDiscount = [0] * (budget + 1)
+            idx = u - 1
+            price_full = present[idx]
+            price_half = math.floor(price_full / 2)
+            
+            prof_full = future[idx] - price_full
+            prof_half = future[idx] - price_half
 
-            for v in tree[u]:
-                if v == parent:
-                    continue 
-                childNoDiscount, childWithDiscount = dp(v, u) 
-                noDiscount = self._merge(noDiscount, childNoDiscount) 
-                withDiscount = self._merge(withDiscount, childWithDiscount) 
+            res_no_discount = [-1] * (budget + 1)
+            res_with_discount = [-1] * (budget + 1)
+            
+            for c in range(budget + 1):
+                if child_sum_if_u_skip[c] != -1:
+                    res_no_discount[c] = max(res_no_discount[c], child_sum_if_u_skip[c])
+            
+            if prof_full > 0: 
+                for c in range(budget + 1 - price_full):
+                    if child_sum_if_u_bought[c] != -1:
+                        res_no_discount[c + price_full] = max(res_no_discount[c + price_full], child_sum_if_u_bought[c] + prof_full)
+            else:
+    
+                 if price_full <= budget:
+                      for c in range(budget + 1 - price_full):
+                        if child_sum_if_u_bought[c] != -1:
+                            res_no_discount[c + price_full] = max(res_no_discount[c + price_full], child_sum_if_u_bought[c] + prof_full)
 
-            newDp0 = noDiscount[:] 
-            newDp1 = noDiscount[:] 
+            for c in range(budget + 1):
+                if child_sum_if_u_skip[c] != -1:
+                    res_with_discount[c] = max(res_with_discount[c], child_sum_if_u_skip[c])
+            
 
-            # 1. Buy current node at full cost (no discount)
-            fullCost = present[u] 
-            for b in range(fullCost, budget + 1):
-                profit = future[u] - fullCost 
-                newDp0[b] = max(newDp0[b], withDiscount[b - fullCost] + profit)
+            if price_half <= budget:
+                for c in range(budget + 1 - price_half):
+                    if child_sum_if_u_bought[c] != -1:
+                        res_with_discount[c + price_half] = max(res_with_discount[c + price_half], child_sum_if_u_bought[c] + prof_half)
+            
+            return res_no_discount, res_with_discount
 
-            # 2. Buy current node at half cost (discounted by parent)
-            halfCost = present[u] // 2 #
-            for b in range(halfCost, budget + 1):
-                profit = future[u] - halfCost 
-                newDp1[b] = max(newDp1[b], withDiscount[b - halfCost] + profit) 
 
-            return newDp0, newDp1 
-        return max(dp(0, -1)[0]) 
-
-    def _merge(self, dpA: list[int], dpB: list[int]) -> list[int]:
-        merged = [-math.inf] * len(dpA)
-        for i, a in enumerate(dpA):
-            for j in range(len(dpA) - i):
-                merged[i + j] = max(merged[i + j], a + dpB[j])
-        return merged
+        final_dp, _ = dfs(1)
+        
+        return max(final_dp)
