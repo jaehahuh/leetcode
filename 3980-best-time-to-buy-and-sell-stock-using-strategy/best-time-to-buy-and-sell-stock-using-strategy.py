59ae0aa8f0b14ab422166138c2514f5264c49a98
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-12-18 15:00:47 +0900

    Time: 210 ms (95.88%) | Memory: 29.9 MB (99.49%) - LeetSync

--- 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.py
+++ 3980-best-time-to-buy-and-sell-stock-using-strategy/best-time-to-buy-and-sell-stock-using-strategy.py
@@ -1,28 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
         n = len(prices)
-        orignal_profit = 0
-        for i in range(n):
-            orignal_profit += prices[i] * strategy[i]
-        
-        max_profit = orignal_profit
-        
-        #prefix_sum
-        buy_delta_values = [-s * p for s, p in zip(strategy, prices)]
-        sell_delta_values = [(1 - s) * p for s, p in zip(strategy, prices)]
+        h = k // 2
 
-        prefix_buy_delta = [0] * (n + 1)
-        for i in range(n):
-            prefix_buy_delta[i+1] = prefix_buy_delta[i] + buy_delta_values[i]
+        sp_values = [s * p for s, p in zip(strategy, prices)]
+        base_profit = sum(sp_values)
         
-        prefix_sell_delta = [0] * (n + 1)
-        for i in range(n):
-            prefix_sell_delta[i+1] = prefix_sell_delta[i] + sell_delta_values[i]
+        max_profit = base_profit
+
+        current_original_window_profit = sum(sp_values[0:k])
+        current_modified_window_profit = sum(prices[h:k])
+
+        current_delta_profit = current_modified_window_profit - current_original_window_profit
+        max_profit = max(max_profit, base_profit + current_delta_profit)
         
-        for i in range(n - k + 1):
-            hold_sum = prefix_buy_delta[i + (k//2)] - prefix_buy_delta[i]
-            sell_sum = prefix_sell_delta[i + k] - prefix_sell_delta[i + (k//2)]
-            current_delta_profit = hold_sum + sell_sum
-            max_profit = max(max_profit, orignal_profit + current_delta_profit)
+        for i in range(1, n - k + 1):
+            current_original_window_profit = (current_original_window_profit - sp_values[i-1]) + sp_values[i+k-1]
+            current_modified_window_profit = (current_modified_window_profit - prices[i + h - 1]) + prices[i + k - 1]
+
+            current_delta_profit = current_modified_window_profit - current_original_window_profit
+            max_profit = max(max_profit, base_profit + current_delta_profit)
             
         return max_profit
