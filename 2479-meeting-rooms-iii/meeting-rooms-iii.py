12856b486a2512b82145b82e4dc10c50414ae371
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2025-12-27 13:05:40 +0900

    Time: 163 ms (85.29%) | Memory: 51.2 MB (88.94%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.py
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.py
@@ -1,33 +1,31 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         meetings.sort()
-        
-        available_rooms = [i for i in range(n)]
-        heapq.heapify(available_rooms)
+        empty_rooms = [i for i in range(n)]
+        heapq.heapify(empty_rooms)
 
         occupied_rooms = []
-        meeting_counts = [0] * n
-
+        count = [0] * n
         for start, end in meetings:
             while occupied_rooms and occupied_rooms[0][0] <= start:
-                _end_time, room_id = heapq.heappop(occupied_rooms)
-                heapq.heappush(available_rooms, room_id)
+                end_time, room_id = heapq.heappop(occupied_rooms)
+                heapq.heappush(empty_rooms, room_id)
 
-            if available_rooms:
-                room_id = heapq.heappop(available_rooms)
+            if empty_rooms:
+                room_id = heapq.heappop(empty_rooms)
                 heapq.heappush(occupied_rooms, (end, room_id))
             else:
                 earliest_end_time, room_id = heapq.heappop(occupied_rooms)
                 new_end = earliest_end_time + (end - start)
                 heapq.heappush(occupied_rooms, (new_end, room_id))
-
-            meeting_counts[room_id] += 1
-
+            
+            count[room_id] += 1
+            
         max_meetings = -1
-        result_room = -1
+        result = -1
         for i in range(n):
-            if meeting_counts[i] > max_meetings:
-                max_meetings = meeting_counts[i]
-                result_room = i
+            if count[i] > max_meetings:
+                max_meetings = count[i]
+                result = i
 
-        return result_room
+        return result
