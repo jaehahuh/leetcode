84ceda0cc98f6b9d0f600f30075bbefd732ebaaa
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-01-01 13:41:52 +0900

    Time: 0 ms (100.00%) | Memory: 17.1 MB (96.19%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,14 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        s = ''
-        lst = []
-        for nums in digits:
-            s += str(nums)
-        
-        num = int(s) + 1
-        s_num = str(num)
-
-        for ch in s_num:
-            lst.append(int(ch))
-
-        return lst
+        s = ''.join(str(num) for num in digits)
+        s_num = str(int(s) + 1)
+        new_digits = list(s_num)
+        return [int(ch) for ch in new_digits] 
