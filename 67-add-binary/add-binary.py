6f6d11cf759134404de22f6c857d12611de9b4a0
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-02-15 20:45:24 +0900

    Time: 0 ms (100.00%) | Memory: 19.5 MB (12.44%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -3,20 +3,16 @@ class Solution:
         result = []
         carry = 0
         i, j = len(a)-1, len(b)-1
-        
-        # Start from the end of both strings
         while i >= 0 or j >= 0 or carry:
-        
-            current_bit_sum = carry  # Initialize current bit sum with carry
+            current_bit_sum = carry
             if i >= 0:
-                current_bit_sum += int(a[i])  # Add current bit of a
+                current_bit_sum += int(a[i])
                 i -= 1
             if j >= 0:
-                current_bit_sum  += int(b[j])  # Add current bit of b
+                current_bit_sum += int(b[j])
                 j -= 1
-        
-            # Calculate current bit and carry for the next place
-            result.append(str(current_bit_sum  % 2))  # Current bit result
-            carry = current_bit_sum // 2   # Carry for the next place
 
-        return ''.join(result[::-1]) # Reverse the result
+            result.append(str(current_bit_sum % 2))  
+            carry = current_bit_sum // 2   
+
+        return ''.join(result[::-1]) 
