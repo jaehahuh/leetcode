cc17fd4e152c13d475ad5bbd49a05e5835698218
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-02-22 15:51:02 +0900

    Time: 0 ms (100.00%) | Memory: 19.6 MB (9.94%) - LeetSync

--- 899-binary-gap/binary-gap.py
+++ 899-binary-gap/binary-gap.py
@@ -1,15 +1,8 @@
 class Solution:
     def binaryGap(self, n: int) -> int:
         binary_num = bin(n)[2:]
-        prev_i = -1
+        positions = [i for i, bit in enumerate(binary_num) if bit == '1']
         longest_distance = 0
-        for i in range(len(binary_num)):
-            cuur_distance = 0
-            if binary_num[i] == '1' and prev_i == -1:
-                prev_i = i
-            elif binary_num[i] == '1' and prev_i != -1:
-                curr_distance = i - prev_i
-                longest_distance = max(longest_distance, curr_distance)
-                prev_i = i
-        
+        for i in range(1, len(positions)):
+            longest_distance = max(longest_distance, positions[i]-positions[i-1])
         return longest_distance
