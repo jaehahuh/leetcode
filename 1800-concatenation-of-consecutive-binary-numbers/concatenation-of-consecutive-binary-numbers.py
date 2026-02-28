0eb1398633700e1b494c64543f24dd7607ca2f84
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-02-28 12:17:57 +0900

    Time: 659 ms (69.18%) | Memory: 19.2 MB (79.87%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.py
@@ -1,8 +1,10 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
         MOD = 10 ** 9 + 7
-        concat_num = ''
-        for i in range(1,n+1):
-            concat_num += bin(i)[2:]
-        
-        return int(concat_num,2)%MOD
+        result = 0
+        length = 0
+        for i in range(1, n+1):
+            if (i & (i-1) == 0): # i가 2의 거듭제곱에 도달하면 이진수 길이 1 증가
+                length += 1
+            result = ((result << length) + i) % MOD
+        return result
