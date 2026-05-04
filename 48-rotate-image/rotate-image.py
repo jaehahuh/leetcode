a3f5c250f54115c095df6ba0e6a8e27d45054f86
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-05-04 11:12:19 +0900

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.02%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        
-        #transpose
-        for i in range(len(matrix)): 
-            for j in range(i, len(matrix)):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i+1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
-        #horizontal Flip after transpose
-        for i in range(len(matrix)):
+        for i in range(n):
             matrix[i].reverse()
