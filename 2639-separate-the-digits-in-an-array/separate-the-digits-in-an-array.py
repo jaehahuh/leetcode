9160abece3611abb35855190b1fa1f77ca5c564e
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-05-11 12:45:03 +0900

    Time: 3 ms (76.03%) | Memory: 19.4 MB (84.71%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
@@ -2,7 +2,11 @@ class Solution:
     def separateDigits(self, nums: List[int]) -> List[int]:
         result = []
         for num in nums:
-            num = str(num)
-            for s in num:
-                result.append(int(s))
+            digits = []
+            while num > 0:
+                digits.append(num%10)
+                num = num//10
+            reverse_order = digits[::-1]
+            result.extend(reverse_order)
+
         return result
