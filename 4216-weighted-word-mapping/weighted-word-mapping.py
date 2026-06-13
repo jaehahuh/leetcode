519fbf3358d8b815fadd8f61966f6e30adf63604
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-06-13 20:17:00 +0900

    Time: 16 ms (8.37%) | Memory: 19.2 MB (94.26%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.py
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -1,19 +1,8 @@
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
-        alpha_map = {}
-        for i in range(26):
-            alpha_map[i] = chr(ord('z') - i)
-        
-        weight_map = {} 
-        for i in range(len(weights)):
-            weight_map[chr(ord('a') + i)] = weights[i]
-        
-        result = ''
+        result = []
         for word in words:
-            total = 0
-            for ch in word:
-                total += weight_map[ch]
-            total = total % 26
-            result += alpha_map[total]
-        
-        return result
+            total_weight = sum(weights[ord(char) - ord('a')] for char in word) % 26
+            ch_map = chr(ord('z') - total_weight)
+            result.append(ch_map)
+        return ''.join(result)
