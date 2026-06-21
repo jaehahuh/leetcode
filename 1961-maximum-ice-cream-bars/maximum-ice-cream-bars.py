268419d06a304cd9970885558c35d286c03287aa
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-06-21 16:48:20 +0900

    Time: 31 ms (99.38%) | Memory: 30.9 MB (81.86%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -1,11 +1,21 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        count = 0
-        costs.sort()
+        max_cost = max(costs)
+        freq = [0] * (max_cost + 1)
         for cost in costs:
-            coins -= cost
-            if coins >= 0:
-                count += 1
-            else:
+            freq[cost] += 1
+
+        max_count = 0
+        for cost in range(1, max_cost + 1):
+            if freq[cost] == 0:
+                continue
+            
+            if coins < cost:
                 break
-        return count
+            
+            buy_count = min(freq[cost], coins//cost)
+
+            max_count += buy_count
+            coins -= cost * buy_count
+
+        return max_count
