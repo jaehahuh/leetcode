e6d36a9cda75939dc8f058d15cf5fe55b39fa716
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-06-30 19:24:45 +0900

    Time: 103 ms (56.31%) | Memory: 19.4 MB (85.27%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,19 +1,15 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        count_map = defaultdict(int) # Dictionary to count 'a, b, c'
-        start = 0  # Start pointer for the sliding window
         result = 0
-        for end in range(len(s)):
-            count_map[s[end]] += 1
+        left = 0
+        count_dict = defaultdict(int)
+        for right in range(len(s)):
+            count_dict[s[right]] += 1
+            while len(count_dict) == 3:
+                result += len(s) - right
+                count_dict[s[left]] -= 1
+                if count_dict[s[left]] == 0:
+                    del count_dict[s[left]]
+                left += 1
 
-            while len(count_map) == 3:
-                result += len(s) - end  # Count all substrings from start to the end of the string
-
-                # Move the left pointer to shrink the window
-                count_map[s[start]] -= 1
-                if count_map[s[start]] == 0:
-                    del count_map[s[start]] # Remove the character from the dictionary if count is zero
-            
-                start += 1
-
-        return result # Total count of valid substrings
+        return result
