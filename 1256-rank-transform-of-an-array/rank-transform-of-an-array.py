45d797dc018a05d09ab10b2b1556a45326daaadd
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-07-12 19:10:50 +0900

    Time: 32 ms (85.13%) | Memory: 37.5 MB (80.09%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.py
@@ -1,12 +1,5 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
         sorted_arr = sorted(set(arr))
-        index_map = defaultdict(int)
-        for i in range(len(sorted_arr)):
-            index_map[sorted_arr[i]] = i+1
-        
-        result = []
-        for num in arr:
-            result.append(index_map[num])
-        
-        return result
+        index_map = {num : i+1 for i, num in enumerate(sorted_arr)}
+        return [index_map[num] for num in arr]
