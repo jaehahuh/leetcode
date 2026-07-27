de0cc48a0893ee1292d02e78eff6dc8171e06ee6
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-07-27 12:58:43 +0900

    Time: 0 ms (100.00%) | Memory: 19.3 MB (68.04%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.py
@@ -1,11 +1,4 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_heap = []
-        for i, num in enumerate(nums):
-            # Using negative values allows the default min-heap to behave like a max-heap
-            heapq.heappush(max_heap, (-num)) 
-        
-        element1 = -heapq.heappop(max_heap)
-        element2 = -heapq.heappop(max_heap)
-        
-        return (element1-1) * (element2-1)
+        nums.sort(reverse=True)
+        return (nums[0]-1) * (nums[1]-1)
