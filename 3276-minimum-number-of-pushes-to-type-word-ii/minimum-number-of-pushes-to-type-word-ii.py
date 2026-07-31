f16b11f0a8d3dfd16a4b11a2375dbfa484b83dbb
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-07-31 13:06:56 +0900

    Time: 100 ms (66.89%) | Memory: 20.2 MB (23.08%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,18 +1,9 @@
 class Solution:
     def minimumPushes(self, word: str) -> int:
         word_counts = Counter(word)
-        sorted_word = [char * count for char, count in word_counts.most_common()]
-        new_word = ''.join(sorted_word)
-
-        n = len(new_word)
-        count_dict = defaultdict(int)
+        sorted_counts = sorted(word_counts.values(), reverse=True)
         result = 0
-        for i in range(n):
-            if new_word[i] not in count_dict:
-                length = len(count_dict)
-                count_dict[new_word[i]] = (length//8) + 1
-                result += (length//8) + 1
-            else:
-                result += count_dict[new_word[i]]
-        
+        for i, count in enumerate(sorted_counts):
+            pushes = (i//8) + 1
+            result += pushes * count
         return result
