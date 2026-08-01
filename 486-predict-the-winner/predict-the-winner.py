2cdb099f2d638de12781dac9b9e920170d77a066
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-08-01 18:53:05 +0900

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.14%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,8 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = list(nums)
+        for i in range(n-2,-1,-1):
+            for j in range(i+1, n):
+                dp[j] = max(nums[i] - dp[j], nums[j] - dp[j-1])
+        return dp[n-1] >= 0
