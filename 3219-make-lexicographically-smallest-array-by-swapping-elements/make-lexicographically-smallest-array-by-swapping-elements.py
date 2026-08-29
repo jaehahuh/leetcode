a4133d35cbb219867576b57f268480491d2b12bb
Jaeha Huh <94806973+jaehahuh@users.noreply.github.com>
2026-08-29 16:16:47 +0900

    Time: 343 ms (35.82%) | Memory: 46.8 MB (79.85%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,17 +1,20 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
-        groups = []
-        num_to_group = {}
+        n = len(nums)
+        index_arr = sorted((num, index) for index, num in enumerate(nums))
+        result = [0] * n
 
-        for num in sorted(nums):
-            if not groups or abs(num - groups[-1][-1]) > limit:
-                groups.append(deque())
-            groups[-1].append(num)
-            num_to_group[num] = len(groups)-1
+        i = 0
+        while i < n:
+            j = i + 1
+            while j < n and index_arr[j][0] - index_arr[j-1][0] <= limit:
+                j += 1
+            
+            indices = sorted(index_arr[k][1] for k in range(i, j))
 
-        result = []
-        for n in nums:
-            i = num_to_group[n]
-            result.append(groups[i].popleft())
-
+            for k in range(i, j):
+                result[indices[k-i]] = index_arr[k][0]
+            
+            i = j
+        
         return result
